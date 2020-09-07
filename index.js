d1ace2a894177ef9699ccad805666664ecfd4f2b
Frank Matranga <thefrankmatranga@gmail.com>
2020-09-07 13:06:28 -0400

    Read bot token from env

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+// Load environment variables from .env file
+const dotenv = require('dotenv')
+dotenv.config()
+
 // Import Discord Node Module
 const Discord = require("discord.js");
 
 // Define Roles with 'id's
 const Roles = [
-    {'name': 'Class21' , 'id': '735896289711095938'},
-    {'name': 'Class22' , 'id': '735896105660842055'},
-    {'name': 'Class23' , 'id': '735896180084572210'},
-    {'name': 'Class24' , 'id': '735896243393265754'},
-    {'name': "Intro", 'id': '749708912021733396'}
+    { 'name': 'Class21', 'id': '735896289711095938' },
+    { 'name': 'Class22', 'id': '735896105660842055' },
+    { 'name': 'Class23', 'id': '735896180084572210' },
+    { 'name': 'Class24', 'id': '735896243393265754' },
+    { 'name': "Intro", 'id': '749708912021733396' }
 ];
 
 // Generate role list
 let roles_name_list = [];
-for(let i = 0; i < Roles.length; i++) {
+for (let i = 0; i < Roles.length; i++) {
     roles_name_list.push(Roles[i]['name']);
 }
 
@@ -22,7 +26,7 @@ const bot = new Discord.Client();
 bot.on('message', (message) => {
     // Div'id'e input into parts
     const parts = message.content.split(' ');
-    
+
     // Help command
     if (parts[0] === "!help") {
         message.channel.send("**List of commands:** \n----------------------------\n`!help` - bring up this prompt\n`!role` <role> - add yourself to a role (use `!role list` to recive a direct message with a list of all of the roles)")
@@ -30,7 +34,7 @@ bot.on('message', (message) => {
 
     // Role command
     else if (parts[0] === "!role") {
-        switch(parts[1]) {
+        switch (parts[1]) {
             case Roles[0]['name'].toString():
                 message.member.roles.add(Roles[0]['id']);
                 message.member.send("Successfully added " + Roles[0]["name"]);
@@ -58,8 +62,8 @@ bot.on('message', (message) => {
                 message.channel.send("Invalid input");
                 break;
         }
-    } 
+    }
 });
 
 // Bot login using key
-bot.login("NzM3NDYwNjM2MzYzMjYwMDE2.Xx9rvQ.8FneFvqiL6CmLMlLPQMiu7CR6Ww");
+bot.login(process.env.DISCORD_BOT_TOKEN);
